72c3416ba7b3deb5a554f83569e8a6c958cb8ad6
shivam rajput <shivam.rajput_cs18@gmail.com>
2020-02-20 16:02:30 +0530

    Add a parametrize constructior

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -7,4 +7,11 @@ public class Person {
     private String lastName;
     private List<Integer> listOfNumbers;
     private String emailId;
+
+    public Person(String firstName, String lastName, List<Integer> listOfNumbers, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.listOfNumbers = listOfNumbers;
+        this.emailId = emailId;
+    }
 }
