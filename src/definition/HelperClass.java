ff00d9d742480127071fa9f236224b96a661b119
shivam rajput <shivam.rajput_cs18@gmail.com>
2020-02-20 16:39:22 +0530

    add constructor and gettrs methods

--- src/definition/HelperClass.java
+++ src/definition/HelperClass.java
@@ -6,5 +6,23 @@ public class HelperClass {
         private Node<E> head;
         private E data;
         private Node<E> next;
+
+        public Node(Node<E> head, E data, Node<E> next) {
+            this.head = head;
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node<E> getHead() {
+            return head;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
     }
 }
