4350473f4ac1bb5ba9c1a4455133e20d24320374
shivam rajput <shivam.rajput_cs18@gmail.com>
2020-02-20 16:01:18 +0530

    Add private fields firstName , lastName , ListOFNumbers , EmailId in Person Class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,4 +1,10 @@
 package definition;
 
+import java.util.List;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private List<Integer> listOfNumbers;
+    private String emailId;
 }
